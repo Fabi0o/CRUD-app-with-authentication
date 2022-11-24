f945c67b1a5a2933f1aec5bbf691bfdac6ad3383
Fabi0o <marfab32@gmail.com>
2022-11-24 21:28:59 +0100

    registration functionality

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
+import Axios from "axios";
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const registerUser = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3001/createUser", {
+      name: email,
+      password: password,
+    }).then(() => {
+      console.log("succes");
+    });
+  };
   return (
-    <form>
+    <form onSubmit={registerUser}>
       <label htmlFor="E-mail">E-mail:</label>
-      <input id="E-mail" type="email" required />
+      <input
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        id="E-mail"
+        type="email"
+        required
+      />
       <label htmlFor="password">Password:</label>
-      <input id="password" type="password" required />
-      <label htmlFor="confPassword">Confirm password:</label>
-      <input id="confPassword" type="password" required />
+      <input
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        id="password"
+        type="password"
+        required
+      />
       <button>Register</button>
     </form>
   );
